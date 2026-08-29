a0da4e3554cb27ec973fc9b94e1d0cac3b4c985f
Yoav Geva <dryoavgeva@gmail.com>
2026-08-29 10:53:27 +0300

    ci: validate against FerricStore 0.11.14

--- fixture_test.go
+++ fixture_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-const pinnedIntegrationServerVersion = "0.11.11"
+const pinnedIntegrationServerVersion = "0.11.14"
 
 func TestIntegrationDockerScriptDefaultsToPinnedFerricStore(t *testing.T) {
 	compose, err := os.ReadFile("docker-compose.yml")
